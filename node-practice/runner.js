e03cb28c26e444474f2c3eb46cdcd7eac446006d
IvanPelivan <pelivaniv@gmail.com>
2022-11-24 16:10:34 +0100

    Update user input
    User can now enter path as an argument before execution of app.

--- node-practice/runner.js
+++ node-practice/runner.js
@@ -2,9 +2,16 @@ const logger = require("./logger");
 const execute = require("child_process").exec;
 const alert = require("alert");
 const prompt = require("prompt-sync")();
+let path = "";
+process.argv[2]
+  ? (path = process.argv[2])
+  : (path = prompt("Please enter path to your file: "));
 
-const path = prompt("Please enter path to your file: ");
-const ext = path.split(".").slice(1).join(".");
+let ext = "";
+const i = path.lastIndexOf(".");
+if (i >= 0) {
+  ext = path.substring(i + 1);
+}
 const supportedExt = ["txt", "png", "jpg", "wav", "mp3"];
 
 if (supportedExt.includes(ext)) {
