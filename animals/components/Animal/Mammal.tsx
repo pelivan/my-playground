7dda9c74d1de2ac934968dfb30c988884cd79238
IvanPelivan <pelivaniv@gmail.com>
2022-12-01 14:48:19 +0100

    Fix console errors
    Fixed all console errors.

--- animals/components/Animal/Mammal.tsx
+++ animals/components/Animal/Mammal.tsx
@@ -29,12 +29,7 @@ function DisplayMammal({
           stroke="currentColor"
           aria-hidden="true"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          />
+          <path d="M6 18L18 6M6 6l12 12" />
         </svg>
       </button>
       <ul>
--- animals/components/BaseComponents/Dropdown.tsx
+++ animals/components/BaseComponents/Dropdown.tsx
@@ -21,11 +21,10 @@ function Dropdown({ dataToMap, label, animalTypeHandler }: DropdownProps) {
         id="livesIn"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
-        <option value="" disabled selected hidden>
-          Please choose...
-        </option>
         {Object.values(dataToMap).map((value) => (
-          <option value={value}>{value}</option>
+          <option key={value} value={value}>
+            {value}
+          </option>
         ))}
       </select>
     </div>
